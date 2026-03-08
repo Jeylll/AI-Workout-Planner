a50bfc2e476aea565843027ee0c027292cd592ad
Jeylll <niceriojayleo@gmail.com>
2026-03-08 13:02:13 +0800

    first commit

---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,20 @@
+import { RedirectToSignIn, SignedIn } from "@neondatabase/neon-js/auth/react";
+import { useAuth } from "../context/AuthContext";
+
+export default function Onboarding() {
+    const { user } = useAuth();
+
+    if (!user) {
+        return <RedirectToSignIn />;
+    }
+
+    return (
+        <SignedIn>
+            <div className="min-h-screen pt-24 pb-12 px-6">
+                <div className="max-w-xl mx-auto">
+
+                </div>
+            </div>
+        </SignedIn>
+    )
+}
